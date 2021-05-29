4f7d62386ba2486fe0508b1dad70e6344b0a6403
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-05-29 19:37:52 +0500

    Added dto model

---
+++ SampleBlazorDashboard/DTOs/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SampleBlazorDashboard.DTOs
+{
+    public class Person
+    {
+        public int Id => new Random().Next(0, 10000);
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
